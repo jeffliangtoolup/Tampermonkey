e1ff86b885586ab7b12550c79d3887f1e0f4ecaf
jeffliangtoolup <86325625+jeffliangtoolup@users.noreply.github.com>
2022-11-08 16:24:55 -0800

    Add files via upload

--- Show Images In ShipHawk.user.js
+++ Show Images In ShipHawk.user.js
@@ -2,7 +2,7 @@
 // @name         Show Images In ShipHawk
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  replaced images in shiphawk
+// @description  Adds images in shiphawk. This adds click to copy UPC and enlarge image.
 // @author       You
 // @match        https://shiphawk.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shiphawk.com
