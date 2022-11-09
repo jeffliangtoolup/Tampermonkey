01d97b5de092baaa723b75984a908b67fa458a9d
jeffliangtoolup <86325625+jeffliangtoolup@users.noreply.github.com>
2022-11-08 16:22:20 -0800

    Add files via upload

--- Show Images In ShipHawk.user.js
+++ Show Images In ShipHawk.user.js
@@ -2,7 +2,7 @@
 // @name         Show Images In ShipHawk
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  replaced images in shiphawk
 // @author       You
 // @match        https://shiphawk.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shiphawk.com
